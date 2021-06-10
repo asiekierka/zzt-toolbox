b2b87dd7c2cd9ecd18c25919d842a2f4887489fe
Adrian Siekierka <kontakt@asie.pl>
2021-06-10 16:24:19 +0200

    zgraph: visual improvements

--- zgraph.py
+++ zgraph.py
@@ -19,6 +19,8 @@
 # SOFTWARE.
 
 #!/usr/bin/env python3
+
+import colorsys
 import os
 import queue
 import sys
@@ -27,6 +29,12 @@ import zookeeper
 import networkx as nx
 import pygraphviz as viz
 
+def has_path_from_start(board_graph, world, c):
+	for idx in c:
+		if nx.has_path(board_graph, world.world.current_board, idx):
+			return True
+	return False
+
 def main():
 	world = zookeeper.Zookeeper(sys.argv[-1])
 	board_graph = nx.DiGraph()
@@ -34,7 +42,9 @@ def main():
 	backtracking_number = 1
 	for idx in range(0, world.world.total_boards):
 		board: zookeeper.Board = world.boards[idx]
-		board_graph.add_node(idx, label=board.title)
+		board_graph.add_node(idx, label=board.title, style="filled", fillcolor="white")
+		if idx == world.world.current_board:
+			board_graph.nodes[idx]["style"] = "filled, bold"
 		board_names[idx] = board.title
 		if board.board_north > 0:
 			board_graph.add_edge(idx, board.board_north)
@@ -47,17 +57,34 @@ def main():
 		for stat in board.stat_data:
 			element: zookeeper.Element = board.get_element((stat.x, stat.y))
 			if element.oop_name.casefold() == 'PASSAGE'.casefold():
-				board_graph.add_edge(idx, stat.param3)
+				board_graph.add_edge(idx, stat.param3, style="dashed")
+
+	clique_graph = nx.Graph()
 	for idx in range(0, world.world.total_boards):
 		board: zookeeper.Board = world.boards[idx]
 		results = {idx: True}
+		clique_graph.add_node(idx)
+		clique_graph.add_edge(idx, idx)
 		for idx2 in range(0, world.world.total_boards):
 			if idx2 != idx and nx.has_path(board_graph, idx, idx2) and nx.has_path(board_graph, idx2, idx):
 				results[idx2] = True
+				clique_graph.add_edge(idx, idx2)
 		board_graph.add_node(idx, label=board.title + (" (%d)" % (len(results))))
 		backtracking_number = max(backtracking_number, len(results))
 
+	cliques = list(sorted(filter(lambda x: has_path_from_start(board_graph, world, x), nx.find_cliques(clique_graph))))
+	hsv_fillcolors = [(x / len(cliques), 0.3, 0.9) for x in range(len(cliques))]
+	rgb_fillcolors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_fillcolors))
+
+	for cid in range(0, len(cliques)):
+		c = cliques[cid]
+		fillcolor = "#%02x%02x%02x" % (round(rgb_fillcolors[cid][0] * 255), round(rgb_fillcolors[cid][1] * 255), round(rgb_fillcolors[cid][2] * 255))
+		for i in c:
+			board_graph.nodes[i]['fillcolor'] = fillcolor
+
 	board_agraph = nx.drawing.nx_agraph.to_agraph(board_graph)
+	# board_agraph.graph_attr["rankdir"] = "LR"
+	board_agraph.add_subgraph([world.world.current_board, 0], rank="same")
 	board_agraph.layout('dot')
 	board_agraph.draw("graph.png")
 
